fe05fb3a88f2ffe52f457c9f10bc2ad1c577ecff
mago-del-caos <profesorxjuvenal@gmail.com>
2026-09-04 19:21:22 -0600

    Update CACHE_NAME to chemlist-v43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chemlist-v41';
+const CACHE_NAME = 'chemlist-v43';
 const urlsToCache = ['./', './index.html', './version.js', './app.js?v=39', './logo.png', './manifest.json'];
 
 self.addEventListener('install', (e) => {
